2d11fb007be1d18b8df3af2bca2b83e8e115067d
Cptnobvious <tylercrawford2@gmail.com>
2014-04-27 04:36:58 -0500

    Can now write character strings to the screen with the text writer class

--- src/util/TextWriter.java
+++ src/util/TextWriter.java
@@ -11,7 +11,6 @@ import static org.lwjgl.opengl.GL11.glTexCoord2f;
 import static org.lwjgl.opengl.GL11.glTranslatef;
 import static org.lwjgl.opengl.GL11.glVertex2i;
 import gzap.Boot;
-import gzap.Standards;
 
 public class TextWriter {
 
@@ -25,6 +24,7 @@ public class TextWriter {
 		float xloc;
 		xloc = 5f / 512f;
 		
+		
 		switch(c){
 		default:
 		case 'a':
@@ -35,10 +35,108 @@ public class TextWriter {
 		case 'B':
 			offset = 1;
 			break;
+		case 'c':
+		case 'C':
+			offset = 2;
+			break;
+		case 'd':
+		case 'D':
+			offset = 3;
+			break;
+		case 'e':
+		case 'E':
+			offset = 4;
+			break;
+		case 'f':
+		case 'F':
+			offset = 5;
+			break;
+		case 'g':
+		case 'G':
+			offset = 6;
+			break;
+		case 'h':
+		case 'H':
+			offset = 7;
+			break;
+		case 'i':
+		case 'I':
+			offset = 8;
+			break;
+		case 'j':
+		case 'J':
+			offset = 9;
+			break;
+		case 'k':
+		case 'K':
+			offset = 10;
+			break;
+		case 'l':
+		case 'L':
+			offset = 11;
+			break;
+		case 'm':
+		case 'M':
+			offset = 12;
+			break;
+		case 'n':
+		case 'N':
+			offset = 13;
+			break;
+		case 'o':
+		case 'O':
+			offset = 14;
+			break;
+		case 'p':
+		case 'P':
+			offset = 15;
+			break;
+		case 'q':
+		case 'Q':
+			offset = 16;
+			break;
+		case 'r':
+		case 'R':
+			offset = 17;
+			break;
+		case 's':
+		case 'S':
+			offset = 18;
+			break;
+		case 't':
+		case 'T':
+			offset = 19;
+			break;
+		case 'u':
+		case 'U':
+			offset = 20;
+			break;
+		case 'v':
+		case 'V':
+			offset = 21;
+			break;
+		case 'w':
+		case 'W':
+			offset = 22;
+			break;
+		case 'x':
+		case 'X':
+			offset = 23;
+			break;
+		case 'y':
+		case 'Y':
+			offset = 24;
+			break;
+		case 'z':
+		case 'Z':
+			offset = 25;
+			break;
 		}
 		
 		return (xloc * offset);
 	}
+	
+	
 	
 	public void drawLetter(int ScreenX, int ScreenY, char c){
 		
@@ -79,7 +177,10 @@ public class TextWriter {
 		
 		for (int x = 0; x < text.length(); x++){
 			char c = text.charAt(x);
-			drawLetter(ScreenX + (x * 10), ScreenY, c);
+			
+			if (c != ' '){
+				drawLetter(ScreenX + (x * 9), ScreenY, c);
+			}
 		}
 		
 	}
